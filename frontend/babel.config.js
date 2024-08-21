64315471ef8a90102c99316b8abac9b6715168b4
Djordje-Stojanovic <djordjestojanovic1992@gmail.com>
2024-08-21 17:41:49 +0200

    Updated dependencies to fix test issue

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,7 +1,10 @@
 module.exports = {
-    presets: [
-      '@babel/preset-env',
-      '@babel/preset-react',
-      '@babel/preset-typescript',
-    ],
-  };
+  presets: [
+    '@babel/preset-env',
+    '@babel/preset-react',
+    '@babel/preset-typescript',
+  ],
+  plugins: [
+    '@babel/plugin-proposal-private-property-in-object'
+  ]
+};
