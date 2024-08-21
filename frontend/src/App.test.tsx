60b75e9a03982031a8ade0c094595442d16acdfc
Djordje-Stojanovic <djordjestojanovic1992@gmail.com>
2024-08-21 17:19:18 +0200

    Updated some frontend tests so CICD Runs correctly

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders welcome message', () => {
+test('renders learn react link', () => {
   render(<App />);
-  const welcomeElement = screen.getByText(/Welcome to Stojanovic-One/i);
-  expect(welcomeElement).toBeInTheDocument();
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
