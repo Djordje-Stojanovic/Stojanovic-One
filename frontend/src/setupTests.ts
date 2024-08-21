97223d135a500c5d4389792a413ceac5c9670978
Djordje-Stojanovic <djordjestojanovic1992@gmail.com>
2024-08-21 17:47:05 +0200

    Finalized the react tests

--- frontend/src/setupTests.ts
+++ frontend/src/setupTests.ts
@@ -1,5 +1,20 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import { configure } from '@testing-library/react';
+
+// Increase the default timeout
+configure({ asyncUtilTimeout: 5000 });
+
+// Suppress act() warnings
+const originalError = console.error;
+beforeAll(() => {
+  console.error = (...args) => {
+    if (/Warning.*not wrapped in act/.test(args[0])) {
+      return;
+    }
+    originalError.call(console, ...args);
+  };
+});
+
+afterAll(() => {
+  console.error = originalError;
+});
