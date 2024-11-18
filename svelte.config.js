85fbd353d113e58f7ba1f0d3adf3447d03cd1068
Djorjde-Stojanovic <djordjestojanovic1992@gmail.com>
2024-11-18 19:37:58 +0100

    Update to Coolify

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import sveltePreprocess from 'svelte-preprocess';
 
@@ -14,9 +14,7 @@ const config = {
 	],
 
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		// Using adapter-node for Coolify deployment
 		adapter: adapter()
 	}
 };
