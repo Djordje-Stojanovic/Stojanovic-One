c0c0e2289c499fb1436a81c6b824d226f11d8ae6
Djorjde-Stojanovic <djordjestojanovic1992@gmail.com>
2024-11-29 17:43:49 +0100

    Updated close chart button

--- src/lib/components/financials/margins/MarginSelector.svelte
+++ src/lib/components/financials/margins/MarginSelector.svelte
@@ -49,7 +49,7 @@ function toggleMargin(margin: MarginOption) {
 $: marginStates = $chartStore.margins;
 </script>
 
-<div class="flex flex-wrap gap-2">
+<div class="flex flex-wrap gap-2 items-center">
     {#each margins as margin}
         <button
             class="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium transition-all duration-200
@@ -64,4 +64,13 @@ $: marginStates = $chartStore.margins;
             {margin.name}
         </button>
     {/each}
+    <button
+        class="ml-auto p-1.5 rounded-full bg-gray-100 dark:bg-gray-700 hover:bg-gray-200 dark:hover:bg-gray-600 transition-colors"
+        on:click={() => chartStore.clearChart()}
+        title="Close Chart"
+    >
+        <svg class="w-4 h-4 text-gray-600 dark:text-gray-300" viewBox="0 0 20 20" fill="currentColor">
+            <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" />
+        </svg>
+    </button>
 </div>
